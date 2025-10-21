5dd12a36ae57f6ab779a43df55aecfcb69b219c8
s24004 <s24004@std.it-college.ac.jp>
2025-10-21 12:00:37 +0900

    リンクが抜けていたので追加

--- app/_components/NewsList/index.tsx
+++ app/_components/NewsList/index.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Link from "next/link";
 
 import styles from "./index.module.css";
 import Category from "../Category";
@@ -18,26 +19,24 @@ export default function NewsList({ news }: Props) {
         <ul>
             {news.map((article) => (
                 <li key={article.id} className={styles.list}>
-                    <div className={styles.link}>
-                        {
-                            article.thumbnail ? (
-                                <Image
-                                    src={article.thumbnail.url}
-                                    alt=""
-                                    className={styles.image}
-                                    width={article.thumbnail.width}
-                                    height={article.thumbnail.height}
-                                />
-                            ) : (
-                                <Image
-                                    className={styles.image}
-                                    src="/no-image.png"
-                                    alt="No Image"
-                                    width={1200}
-                                    height={630}
-                                />
-                            )
-                        }
+                    <Link href={`/news/${article.id}`} className={styles.link}>
+                        {article.thumbnail ? (
+                            <Image
+                                src={article.thumbnail.url}
+                                alt=""
+                                className={styles.image}
+                                width={article.thumbnail.width}
+                                height={article.thumbnail.height}
+                            />
+                        ) : (
+                            <Image
+                                src="/no-image.png"
+                                alt=""
+                                className={styles.image}
+                                width={1200}
+                                height={630}
+                            />
+                        )}
                         <dl className={styles.content}>
                             <dt className={styles.title}>{article.title}</dt>
                             <dd className={styles.meta}>
@@ -45,7 +44,8 @@ export default function NewsList({ news }: Props) {
                                 <Date date={article.publishedAt ?? article.createdAt} />
                             </dd>
                         </dl>
-                    </div>
+                    </Link>
+                    
                 </li>
             ))}
         </ul>
