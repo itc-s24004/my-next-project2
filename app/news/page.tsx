1ee28cd617775b14bdae3e4be45842ce2348cc82
s24004 <s24004@std.it-college.ac.jp>
2025-10-23 11:46:53 +0900

    検索機能を実装

--- app/news/page.tsx
+++ app/news/page.tsx
@@ -1,5 +1,6 @@
 import NewsList from "../_components/NewsList";
 import Pagination from "../_components/Pagination";
+import SearchField from "../_components/SearchField";
 import { NEWS_LIST_LIMIT } from "../_constants";
 import { getNewsList } from "../_libs/microcms";
 
@@ -12,9 +13,10 @@ export default async function Page() {
 
     return (
         <>
+            <SearchField />
             <NewsList news={news} />
             <Pagination totalCount={totalCount} />
         </>
-    )
+    );
     
 }
